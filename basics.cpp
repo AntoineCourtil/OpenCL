75c94b66ea04c443717d8158eac99c2480227544
AntoineCourtil <courtil.antoine@hotmail.fr>
2018-03-30 14:57:25 +0200

    init project

---
+++ basics.cpp
@@ -0,0 +1,28 @@
+// ----------------------------------------------------------
+
+#include <cstdio>
+#include <cstdlib>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+// ----------------------------------------------------------
+
+// We provide a small library so we can easily setup OpenCL
+#include "clutils.h"
+
+// ----------------------------------------------------------
+
+int main(int argc,char **argv)
+{
+
+  const char * clu_file = SRC_PATH "basics.cl";
+
+  return 0;
+}
+
+// ----------------------------------------------------------
+

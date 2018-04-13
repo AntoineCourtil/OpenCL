47c0d35783b936dcf669ad442069d19cec3caead
AntoineCourtil <courtil.antoine@hotmail.fr>
2018-04-13 17:26:02 +0200

    v3 bug

--- basics.cl
+++ basics.cl
@@ -1,9 +1,9 @@
 
-__kernel void reduceV1(__global const int *a, __global int *b, int i){
+__kernel void reduceV1(__global const int *a, __global int *b, int i) {
 
     int tid = get_global_id(0);
-    int u = tid * (1<<i);
-    int v = tid * (1<<i) + (1<<(i-1));
+    int u = tid * (1 << i);
+    int v = tid * (1 << i) + (1 << (i - 1));
 
     int sum = a[u] + a[v];
 
@@ -12,7 +12,7 @@ __kernel void reduceV1(__global const int *a, __global int *b, int i){
 }
 
 
-__kernel void reduceV2(__global const int *a, __global int *b, int i){
+__kernel void reduceV2(__global const int *a, __global int *b, int i) {
 
     int tid = get_global_id(0);
     int u = tid * 2;
@@ -21,5 +21,37 @@ __kernel void reduceV2(__global const int *a, __global int *b, int i){
     int sum = a[u] + a[v];
 
     b[tid] = sum;
+
+}
+
+
+__kernel void reduceV3(__global int *a, __global int *b, int i) {
+
+    int tid = get_global_id(0);
+    int lid = get_local_id(0);
+    int gid = get_group_id(0);
+
+    for (int j = 1; j <= 9; j++) {
+        int v0, v1;
+        bool active = lid < (1 << (9 - j));
+
+
+        if (active) { //Thread active ?
+            //read values
+            v0 = a[gid * 512 + lid * 2];
+            v1 = a[gid * 512 + lid * 2 + 1];
+        }
+
+        barrier(CLK_GLOBAL_MEM_FENCE);
+
+        if (active) { //Thread active ?
+            //write value
+            a[gid * 512 + lid] = v0 + v1;
+        }
+
+        barrier(CLK_GLOBAL_MEM_FENCE);
+    }
+
+    b[gid] = a[gid * 512];
 
 }
--- basics.cpp
+++ basics.cpp
@@ -95,18 +95,19 @@ int main(int argc, char **argv) {
     int sumV1 = 0;
     clu_Queue->enqueueReadBuffer(*buffer[swap], true, 0, sizeof(int), &sumV1);
 
-    cerr << "   sumV1 : " << sumV1 <<endl;
-    
-    
-    
-    
-    
-    
-    
+    cerr << "   sumV1 : " << sumV1 << endl;
+
+
+
+
+
+
+
     ///////////////////////////////////////////
     ///                 V2                  ///
     ///////////////////////////////////////////
 
+    swap = 0;
 
     cerr << endl << endl;
 
@@ -114,11 +115,6 @@ int main(int argc, char **argv) {
     clu_Queue->enqueueWriteBuffer(*buffer[swap], false, 0, n * sizeof(int), table);
 
     cl::Kernel *krnV2 = cluLoadKernel(prg, "reduceV2");
-
-    //Init du tableau
-    for (int i = 0; i < n; i++) {
-        table[i] = 1;
-    }
 
     double total_chrono_V2 = 0;
 
@@ -166,20 +162,77 @@ int main(int argc, char **argv) {
     int sumV2 = 0;
     clu_Queue->enqueueReadBuffer(*buffer[swap], true, 0, sizeof(int), &sumV2);
 
-    cerr << "   sumV2 : " << sumV2 <<endl;
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    cerr << "   sumV2 : " << sumV2 << endl;
+
+
+
+
+
+
+
+    ///////////////////////////////////////////
+    ///                 V3                  ///
+    ///////////////////////////////////////////
+
+    cerr << endl << endl;
+
+    swap = 0;
+    p2 = 9*3; // 9 = 512
+    n = (1 << p2);
+
+    if (n % 256 != 0) {
+        cerr << "error" << endl;
+        return -1;
+    }
+
+    //init des buffers avec les tableaux
+    clu_Queue->enqueueWriteBuffer(*buffer[swap], false, 0, n * sizeof(int), table);
+
+    cl::Kernel *krnV3 = cluLoadKernel(prg, "reduceV3");
+
+    double total_chrono_V3 = 0;
+
+
+    for (int i = n; i >= 512; i = i / 512) {
+        krnV3->setArg(0, *buffer[swap]);
+        krnV3->setArg(1, *buffer[1 - swap]);
+        krnV3->setArg(2, i);
+
+
+        int numT = i / 2;
+        int numG = 256;
+
+
+        cl::Event ev;
+
+        //Ordre par file de commande
+        cl_int err = clu_Queue->enqueueNDRangeKernel(
+                *krnV3, //kernel
+                cl::NullRange, //NullRange
+                cl::NDRange(numT), //NB de Threads
+                cl::NDRange(numG), //Taille de groupe, n doit etre un multiple de taille de groupe
+                0,
+                &ev //Event de mesure de performances
+        );
+
+
+        cluCheckError(err, "Error executing kernel");
+
+        ev.wait();
+
+        total_chrono_V3 += cluEventMilliseconds(ev);
+
+
+        swap = 1 - swap;
+    }
+
+
+    cerr << endl << "[+] V3, total time : " << total_chrono_V3 << endl;
+
+    int sumV3 = 0;
+    clu_Queue->enqueueReadBuffer(*buffer[swap], true, 0, sizeof(int), &sumV3);
+
+    cerr << "   sumV3 : " << sumV3 << endl;
 
 
     delete[](table);

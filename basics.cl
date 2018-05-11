ae4f28dab5c68f99ac12b88dc7bb424b72cf5bab
AntoineCourtil <courtil.antoine@hotmail.fr>
2018-05-11 16:18:26 +0200

    OddEven

--- basics.cl
+++ basics.cl
@@ -1,61 +1,29 @@
 
-__kernel void wsumsV1(__global const int *a, __global int *b, int n, int k) {
+__kernel void odd(__global int *table, int n) {
 
-    int id = get_global_id(0);
+    int id1 = get_global_id(0) * 2 + 1;
+    int id2 = id1 + 1;
 
-    int wsum = 0;
+    int swap;
 
-    for (int j = -k; j <= k; j++) {
-        wsum += a[(id + j) % n];
+    if(table[id2] > table[id1]){
+        swap = table[id2];
+        table[id2] = table[id1];
+        table[id1] = swap;
     }
-
-    b[id] = wsum;
-
 }
 
 
-__kernel void wsumsV2(__global const int *a, __global int *b, __local int *tmp, int n, int k, int groupSize) {
+__kernel void even(__global int *table, int n) {
 
-    int id = get_global_id(0);
-    int lid = get_local_id(0);
-    int gid = get_group_id(0);
+    int id1 = get_global_id(0) * 2;
+    int id2 = id1 + 1;
 
-    int wsum = 0;
+    int swap;
 
-
-
-    if(gid == 0 && lid == 0) {
-        for (int i = 0; i <= k; i++) {
-            tmp[i] = 0;
-            tmp[groupSize-i] = a[id+k+groupSize+i];
-        }
-    } else {
-
-        if(lid == 0){
-            for (int i = 0; i <= k; i++) {
-                tmp[i] = a[id-k+i];
-
-                tmp[k+groupSize+i] = 999999999999999;
-            }
-        }
-
+    if(table[id2] > table[id1]){
+        swap = table[id2];
+        table[id2] = table[id1];
+        table[id1] = swap;
     }
-
-
-
-
-    tmp[lid + k] = a[id];
-
-    barrier(CLK_LOCAL_MEM_FENCE);
-
-
-    for (int j = -k; j <= k; j++) {
-        wsum += tmp[(lid + k + j) % groupSize];
-    }
-
-
-    barrier(CLK_LOCAL_MEM_FENCE);
-
-    b[id] = wsum;
-
 }
--- basics.cpp
+++ basics.cpp
@@ -23,64 +23,99 @@ int main(int argc, char **argv) {
     cluInit();
 
     cl::Program *prg = cluLoadProgram(clu_file);
-    cl::Kernel *kernel = cluLoadKernel(prg, "wsumsV1");
+    cl::Kernel *kernelEven = cluLoadKernel(prg, "even");
+    cl::Kernel *kernelOdd = cluLoadKernel(prg, "odd");
 
-    int p2 = 20;
-    int n = (1 << p2);
-    int k = 128;
-    int groupSize = (1 << 10);
+    int n = 24;
+    int groupSize = 4;
 
     //création du buffer = allocation mémoire du GPU
-    cl::Buffer bufferA(*clu_Context, CL_MEM_READ_WRITE, n * sizeof(int));
-    cl::Buffer bufferB(*clu_Context, CL_MEM_READ_WRITE, n * sizeof(int));
+    cl::Buffer buffer(*clu_Context, CL_MEM_READ_WRITE, n * sizeof(int));
 
-    kernel->setArg(0, bufferA);
-    kernel->setArg(1, bufferB);
-    kernel->setArg(2, n);
-    kernel->setArg(3, k);
+    kernelEven->setArg(0, buffer);
+    kernelEven->setArg(1, n);
+
+    kernelOdd->setArg(0, buffer);
+    kernelOdd->setArg(1, n);
 
     int *table = new int[n];
-    int *wsums = new int[n];
+
+    double total_chrono = 0;
 
 
     //Init du tableau
     for (int i = 0; i < n; i++) {
         table[i] = i;
-        wsums[i] = 0;
     }
 
 
     //init des buffers avec les tableaux
-    clu_Queue->enqueueWriteBuffer(bufferA, true, 0, n * sizeof(int), table);
-    clu_Queue->enqueueWriteBuffer(bufferB, true, 0, n * sizeof(int), wsums);
+    clu_Queue->enqueueWriteBuffer(buffer, true, 0, n * sizeof(int), table);
 
 
-    cl::Event ev;
+    for (int i = 0; i < n; i++) {
 
-    //Ordre par file de commande
-    cl_int err = clu_Queue->enqueueNDRangeKernel(
-            *kernel, //kernel
-            cl::NullRange, //NullRange
-            cl::NDRange(n), //NB de Threads
-            cl::NDRange(groupSize), //Taille de groupe, n doit etre un multiple de taille de groupe
-            0,
-            &ev //Event de mesure de performances
-    );
+        if (i % 2 == 0) {
 
+            cl::Event ev;
 
-    cluCheckError(err, "Error executing kernel");
-
-    ev.wait();
-
-    cluDisplayEventMilliseconds("[+] kernel time", ev);
-    cerr << endl;
+            //Ordre par file de commande
+            cl_int err = clu_Queue->enqueueNDRangeKernel(
+                    *kernelEven, //kernelOdd
+                    cl::NullRange, //NullRange
+                    cl::NDRange(n / 2), //NB de Threads
+                    cl::NDRange(groupSize), //Taille de groupe, n doit etre un multiple de taille de groupe
+                    0,
+                    &ev //Event de mesure de performances
+            );
 
 
-    clu_Queue->enqueueReadBuffer(bufferB, true, 0, n * sizeof(int), wsums);
+            cluCheckError(err, "Error executing kernelOdd");
+
+            ev.wait();
+
+//            cluDisplayEventMilliseconds("[+] kernelOdd time", ev);
+//            cerr << endl;
+
+            total_chrono += cluEventMilliseconds(ev);
+
+        } else {
+
+            cl::Event ev;
+
+            //Ordre par file de commande
+            cl_int err = clu_Queue->enqueueNDRangeKernel(
+                    *kernelOdd, //kernelOdd
+                    cl::NullRange, //NullRange
+                    cl::NDRange(n / 2), //NB de Threads
+                    cl::NDRange(groupSize), //Taille de groupe, n doit etre un multiple de taille de groupe
+                    0,
+                    &ev //Event de mesure de performances
+            );
 
 
-    for (int i = 1000; i < 1028; i++) {
-        cerr << wsums[i] << " " << endl;
+            cluCheckError(err, "Error executing kernelOdd");
+
+            ev.wait();
+
+//            cluDisplayEventMilliseconds("[+] kernelOdd time", ev);
+//            cerr << endl;
+
+            total_chrono += cluEventMilliseconds(ev);
+
+        }
+
+    }
+
+
+    cerr << "[+] Total time : " << total_chrono << " ms" << endl << endl << endlg;
+
+
+    clu_Queue->enqueueReadBuffer(buffer, true, 0, n * sizeof(int), table);
+
+
+    for (int i = 0; i < n; i++) {
+        cerr << table[i] << " " << endl;
     }
 
 
@@ -88,61 +123,10 @@ int main(int argc, char **argv) {
     ///             V2              ///
     ///////////////////////////////////
 
-    cerr << endl << endl << "#####   V2   #####" << endl << endl << endl;
 
-
-    kernel = cluLoadKernel(prg, "wsumsV2");
-
-    kernel->setArg(0, bufferA);
-    kernel->setArg(1, bufferB);
-    kernel->setArg(2, cl::__local((groupSize + (2 * k)) * sizeof(int)));
-    kernel->setArg(3, n);
-    kernel->setArg(4, k);
-    kernel->setArg(5, groupSize);
-
-
-    //Init du tableau
-    for (int i = 0; i < n; i++) {
-        table[i] = i;
-        wsums[i] = 0;
-    }
-
-
-    //init des buffers avec les tableaux
-    clu_Queue->enqueueWriteBuffer(bufferA, true, 0, n * sizeof(int), table);
-    clu_Queue->enqueueWriteBuffer(bufferB, true, 0, n * sizeof(int), wsums);
-
-
-
-    //Ordre par file de commande
-    err = clu_Queue->enqueueNDRangeKernel(
-            *kernel, //kernel
-            cl::NullRange, //NullRange
-            cl::NDRange(n), //NB de Threads
-            cl::NDRange(groupSize), //Taille de groupe, n doit etre un multiple de taille de groupe
-            0,
-            &ev //Event de mesure de performances
-    );
-
-
-    cluCheckError(err, "Error executing kernel");
-
-    ev.wait();
-
-    cluDisplayEventMilliseconds("[+] kernel time", ev);
-    cerr << endl;
-
-
-    clu_Queue->enqueueReadBuffer(bufferB, true, 0, n * sizeof(int), wsums);
-
-
-    for (int i = 1000; i < 1028; i++) {
-        cerr << wsums[i] << " " << endl;
-    }
 
 
     delete[](table);
-    delete[](wsums);
 
 
     return 0;
